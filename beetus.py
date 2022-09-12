cff98919668e7c467a30c2c5b767fcb59c73028b
DJ <heyymrdj@protonmail.com>
2022-09-11 22:56:08 -0400

    Added flask

--- beetus.py
+++ beetus.py
@@ -2,6 +2,7 @@
 # pylint: disable=E0401:import-error
 import sqlite3
 import plotly.express as px
+from flask import Flask
 
 
 DATABASE = "beetus.db"
@@ -28,7 +29,7 @@ def select_all_entries(conn):
     for row in rows:
         print(row)
 
-# print list of entries
+# Print list of all date readings and return list
 def select_all_date(conn):
     'test'
 
@@ -41,7 +42,7 @@ def select_all_date(conn):
     for row in rows:
         x.append(row[0])
     return x
-
+# Print list of all glucose readings and return list
 def select_all_glucose(conn):
     'test'
 
@@ -70,12 +71,23 @@ def generate_graph(date_reading, gluclose_reading):
                     },
                     title="Blood Glucose Chart")
     fig.show()
+    fig.write_html("./static/graph.html")
 
 connection = create_connection(DATABASE)
-
 date = select_all_date(connection)
 glucose = select_all_glucose(connection)
-
 generate_graph(date, glucose)
+
 # Write reading to HTML page
-#fig.write_html("graph.html")
+
+path = "./graph.html"
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    'test'
+    #return "<p>Hello, World!</p>"
+    #return url_for('static', filename="graph.html")
+    return app.send_static_file("graph.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
