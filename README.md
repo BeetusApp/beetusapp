059327df0cb941c1baeb60a71e4f9e179e33f686
DJ <heyymrdj@protonmail.com>
2022-09-11 22:26:33 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # beetusapp
 Diabetes tracking software developed by diabetic(s) for diabetics
+
+## Summary
+This application will allow blood glucose entries to be added for tracking, graphing, and analytic purposes. 
---
+++ beetus.py
@@ -0,0 +1,75 @@
+"This is the main module for the beetus application"
+# pylint: disable=E0401:import-error
+import sqlite3
+import plotly.express as px
+
+
+DATABASE = "beetus.db"
+def create_connection(data_base):
+    'Function to connect to database'
+    conn = None
+    connection_error = None
+    try:
+        conn = sqlite3.connect(data_base)
+    except connection_error as error:
+        print(error)
+
+    return conn
+
+# print list of entries
+def select_all_entries(conn):
+    'test'
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM entries")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+# print list of entries
+def select_all_date(conn):
+    'test'
+
+    x = []
+    cur = conn.cursor()
+    cur.execute("SELECT date FROM entries")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        x.append(row[0])
+    return x
+
+def select_all_glucose(conn):
+    'test'
+
+    y = []
+    cur = conn.cursor()
+    cur.execute("SELECT glucose FROM entries")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        y.append(row[0])
+    return y
+
+connection = create_connection(DATABASE)
+
+date = select_all_date(connection)
+glucose = select_all_glucose(connection)
+# write entries to DB
+
+
+
+#Graph and display readings
+fig = px.line(x=date, y=glucose, template="plotly_dark",
+                 labels={
+                     "x": "Date and Time",
+                     "y": "Blood Glucose Reading",
+                 },
+                title="Blood Glucose Chart")
+fig.show()
+# Write reading to HTML page
+#fig.write_html("graph.html")
